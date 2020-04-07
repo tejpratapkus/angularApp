a89400aeba589f2cdebf5e1c20f240a91a07f7c5
Tejpratap Kushwaha <tkushwaha@XEBIAINDIA.LOCAL>
2020-04-08 01:25:26 +0800

    retrive value in template driven form

--- src/app/templet-form/templet-form.component.ts
+++ src/app/templet-form/templet-form.component.ts
@@ -10,10 +10,35 @@ export class TempletFormComponent implements OnInit {
 
   @ViewChild('myForm') myForm:NgForm;
   defaultCourse = 'Angular';
+  defaultGender = 'Male';
   username = '';
+  submitted = false;
+
+  genders = [
+    {id:1, value:'Male'},
+    {id:1, value:'Female'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setUserName() {
+  /*  this.myForm.setValue({
+      userDetails:  {
+        username: 'Tej',
+        email: ''
+      },
+      course: 'Angular',
+      gender: 'Male'
+    });*/
+
+    this.myForm.form.patchValue({
+      userDetails:  {
+        username: 'Tej'
+      }
+    });
   }
 
   /*onSubmit(form:HTMLFontElement) {
@@ -26,10 +51,25 @@ export class TempletFormComponent implements OnInit {
     console.log(form);
   } */
 
+  formData = {
+    username: '',
+    email: '',
+    course: '',
+    gender: ''
+  }
+
   //View child
   onSubmit() {
     //console.log("submitted");
     console.log(this.myForm);
+    this.submitted = true;
+
+    this.formData.username = this.myForm.value.userDetails.username;
+    this.formData.email = this.myForm.value.userDetails.email;
+    this.formData.course = this.myForm.value.course;
+    this.formData.gender = this.myForm.value.gender;
+
+    this.myForm.reset();
   }
 
 }
